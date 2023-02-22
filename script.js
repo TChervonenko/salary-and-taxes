7a14766a86a6f5b119a7d3cf299f77baca8cac49
TChervonenko <chervonenko.t@gmail.com>
2023-02-22 21:22:14 +0200

    Made changes js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function taxesCalculations () {
     let result = document.querySelector('.result');
     let salary = document.getElementById('salary').value;
-    let taxMilitary = salary/0.805*0.015;
-    let taxIndividuals = salary/0.805*0.18;
+    const bruttoSalary = 0.805;
+    const percentTaxMilitary = 0.015;
+    const percentTaxIndividuals = 0.18;
+    let taxMilitary = salary/bruttoSalary*percentTaxMilitary;
+    let taxIndividuals = salary/bruttoSalary*percentTaxIndividuals;
 
     result.innerHTML = `
     <p>Ваша зарплата за рік:${salary}</p>
