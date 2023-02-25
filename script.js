b078165abc74239a2f9d97495e907672da77e31c
TChervonenko <chervonenko.t@gmail.com>
2023-02-25 13:17:44 +0200

    Added function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
   function calcMilitaryTax(salary, bruttoSalary, percentTaxMilitary) {
-    return salary / bruttoSalary * percentTaxMilitary;
+    return (salary / bruttoSalary * percentTaxMilitary).toFixed(2);
   }
 
   function calcIndividualTax(salary, bruttoSalary, percentTaxIndividuals) {
-    return salary/bruttoSalary*percentTaxIndividuals;
+    return (salary/bruttoSalary*percentTaxIndividuals).toFixed(2);
   }
 
 
